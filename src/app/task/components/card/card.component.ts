a0948bb080d26926870ea071fc54139c33bdd6e3
Thomas K <thmskffr@gmail.com>
2023-02-03 15:07:06 +0100

    displaying toDos

--- src/app/task/components/card/card.component.ts
+++ src/app/task/components/card/card.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { StateTask } from 'src/app/core/enums/state-task';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
+  @Input() title!: string;
+  @Input() category!: StateTask;
+  @Input() categories!: StateTask[];
+//  @Input() item!: Task;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
